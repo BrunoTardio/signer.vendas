306555981214441b2598d07ec2a20844fcb7ac1e
Bruno Tardio <brunotardiosi@gmail.com>
2019-02-21 13:42:15 -0300

    Atualizacao de classes de dominio

--- src/main/java/com/signer/vendas/domain/ProdutoCategoria.java
+++ src/main/java/com/signer/vendas/domain/ProdutoCategoria.java
@@ -3,7 +3,7 @@ package com.signer.vendas.domain;
 import java.io.Serializable;
 
 public class ProdutoCategoria implements Serializable {
-	
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String nome;
