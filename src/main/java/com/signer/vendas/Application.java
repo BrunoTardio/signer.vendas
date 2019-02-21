cba26d7767898685c54d9fe0c72c24efee8c01af
Bruno Tardio <brunotardiosi@gmail.com>
2019-02-21 19:20:12 -0300

    inserindo objetos para teste

--- src/main/java/com/signer/vendas/Application.java
+++ src/main/java/com/signer/vendas/Application.java
@@ -1,13 +1,47 @@
 package com.signer.vendas;
 
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import com.signer.vendas.domain.ProdutoCategoria;
+import com.signer.vendas.repository.ProdutoCategoriaRepository;
 
+
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	
+	@Autowired
+	ProdutoCategoriaRepository pcrepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		ProdutoCategoria pc1 = new ProdutoCategoria(null,"CD");
+		ProdutoCategoria pc2 = new ProdutoCategoria(null,"PEN DRIVE");
+		ProdutoCategoria pc3 = new ProdutoCategoria(null,"MIDIA DIGITAL");
+		
+		pcrepo.saveAll(Arrays.asList(pc1,pc2,pc3));
+	}
+
+
+
+
+
+	
+	
 
 }
