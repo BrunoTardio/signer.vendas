704c8aca920cd28572cabd5984607bec91dd64c6
Bruno Tardio <brunotardiosi@gmail.com>
2019-02-23 11:28:21 -0300

    Endpoint get

--- src/main/java/com/signer/vendas/resource/ClientePFResource.java
+++ src/main/java/com/signer/vendas/resource/ClientePFResource.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.signer.vendas.domain.ClientePF;
@@ -16,7 +17,7 @@ public class ClientePFResource {
 	@Autowired
 	private ClientePFService service;
 
-	@RequestMapping(value = "/{id}")
+	@RequestMapping(value = "/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		ClientePF obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
--- src/main/java/com/signer/vendas/resource/ClientePJResource.java
+++ src/main/java/com/signer/vendas/resource/ClientePJResource.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.signer.vendas.domain.ClientePJ;
@@ -16,7 +17,7 @@ public class ClientePJResource {
 	@Autowired
 	private ClientePJService service;
 	
-	@RequestMapping(value="/{id}")
+	@RequestMapping(value = "/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id){
 		ClientePJ obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
