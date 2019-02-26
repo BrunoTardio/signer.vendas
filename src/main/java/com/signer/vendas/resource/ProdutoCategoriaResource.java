4e2ff02bbb701dfc03e3677e0e60bb9ee1a989d3
Bruno Tardio <brunotardiosi@gmail.com>
2019-02-26 15:26:53 -0300

    Delete produto descricao

--- src/main/java/com/signer/vendas/resource/ProdutoCategoriaResource.java
+++ src/main/java/com/signer/vendas/resource/ProdutoCategoriaResource.java
@@ -27,7 +27,7 @@ public class ProdutoCategoriaResource {
 	ProdutoCategoriaService service;
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<ProdutoCategoria> find(@PathVariable Integer id) {
 		ProdutoCategoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
@@ -46,4 +46,11 @@ public class ProdutoCategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
