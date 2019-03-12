d0b2a7237254cbd3443b23e2afdcc43862eaf8c1
Bruno Tardio <brunotardiosi@gmail.com>
2019-03-12 16:11:36 -0300

    Page com parametros na requisicao

--- src/main/java/com/signer/vendas/service/ProdutoCategoriaService.java
+++ src/main/java/com/signer/vendas/service/ProdutoCategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.signer.vendas.domain.ProdutoCategoria;
@@ -48,6 +51,13 @@ public class ProdutoCategoriaService {
 	
 	public List<ProdutoCategoria> findAll(){
 		return repo.findAll();
+	}
+	
+	 
+	public Page<ProdutoCategoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction ){
+		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 }
